a6dcc68172da71e2554c20da63014bf7a51ed9c3
Marouane Annaim <marouane.annaim@ulb.be>
2023-12-06 16:15:33 +0000

    Remplacer main.c

--- serveur/main.c
+++ serveur/main.c
@@ -1,48 +1,91 @@
 /**
- * Un main avec quelques exemples pour vous aider. Vous pouvez
- * bien entendu modifier ce fichier comme bon vous semble.
+ * Code du serveur 
+ * .
  **/
 
 #include <stdio.h>
 #include <inttypes.h>
 #include <signal.h>
-
 #include "imgdist.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "../commun/commun.h"
+#include <asm-generic/socket.h>
+
+
 
 void ExempleSignaux(void);
 
-int main(int argc, char* argv[]) {
+
+int main(int argc, char* argv[]){
+   int server_fd = checked(socket(AF_INET, SOCK_STREAM, 0));
+
+   int opt = 1;
+   setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
+   struct sockaddr_in address;   //Adresse du serveur du socket
+   address.sin_family = AF_INET;
+   address.sin_addr.s_addr = INADDR_ANY;
+   address.sin_port = htons(5555);
+
+
+   checked(bind(server_fd, (struct sockaddr *)&address, sizeof(address))); //Lie l'adresse au socket
+
+   checked(listen(server_fd, 10)); // mise en écoute de l'utilisateur
+
+
+   size_t addrlen = sizeof(address);
+   int new_socket = checked(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
+  
+   char imagePath[1024];
+   int lu;
+   uint64_t imageHash, hash2;
+
+
+   while ((lu = read(new_socket, imagePath, 1024)) > 0) {
+      printf("Je rentre dans le serveur.\n");
+      printf("Le chemin est %s\n", imagePath);
+
+
+      // Calcule du code de hachage perceptif de l'image reçue via le socket par le client et
+      // conservation de celui-ci dans imageHash.
+      if (!PHash(imagePath, &imageHash))
+         return 1; // Échec dans le chargement de l'image (message sur stderr automatique)
+
+        // Idem pour "img/2.bmp".
+      if (!PHash("img/2.bmp", &hash2))
+         return 1; 
+
+      
+
+      // Calculer la distance entre hash1 et hash2
+      unsigned int distance = DistancePHash(imageHash, hash2); 
+
+      // Afficher la distance entre les deux images (valeur de retour d'img-dist dans le projet 1
+      // quand il n'y avait pas d'erreur).
+      printf("La distance entre les images est : %d\n", distance);
    
-   /// Exemple d'utilisation de la biliothèque img-dist ///
-   uint64_t hash1, hash2;
+      /// Exemple gestion de signaux (cf Annexe de l'énoncé & corrigé du projet 1) ///
    
-   // Calcule du code de hachage perceptif de l'image "img/1.bmp" et
-   // conservation de celui-ci dans hash1.
-   if (!PHash("img/1.bmp", &hash1))
-      return 1; // Échec dans le chargement de l'image (message sur stderr automatique)
+      ExempleSignaux();
    
-   // Idem pour "img/2.bmp".
-   if (!PHash("img/2.bmp", &hash2))
-      return 1;
-   
-   // Calculer la distance entre hash1 et hash2
-   unsigned int distance = DistancePHash(hash1, hash2);
-   
-   // Afficher la distance entre les deux images (valeur de retour d'img-dist dans le projet 1
-   // quand il n'y avait pas d'erreur).
-   printf("Distance entre les images 'img/1.bmp' et 'img/2.bmp' : %d\n", distance);
-   
-   /// Fin de l'exemple d'utilisation de la biliothèque img-dist ///
-   
-   /// Exemple gestion de signaux (cf Annexe de l'énoncé & corrigé du projet 1) ///
-   
-   ExempleSignaux();
-   
-   /// ///
-   
-   
-   
-   return 0;
+      /// ///
+
+
+      checked_wr(write(new_socket, imagePath, lu) < 0);
+  }
+  
+   close(server_fd);
+   close(new_socket);
+
+
+
+
+  return 0;
 }
 
 static volatile sig_atomic_t signalRecu = 0;
@@ -86,57 +129,4 @@ void ExempleSignaux(void) {
    }
    
    /// ///
-}
-
-
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include "../commun/commun.h"
-#include <asm-generic/socket.h>
-
-
-
-int main(){
-  int server_fd = checked(socket(AF_INET, SOCK_STREAM, 0));
-
-  int opt = 1;
-  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-
-  struct sockaddr_in address;   //Adresse du serveur du socket
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(5555);
-
-
-  checked(bind(server_fd, (struct sockaddr *)&address, sizeof(address))); //Lie l'adresse au socket
-
-  checked(listen(server_fd, 10)); // mise en écoute de l'utilisateur
-
-
-  size_t addrlen = sizeof(address);
-  int new_socket = checked(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
-  
-  char buffer[1024];
-  int lu;
-
-  while ((lu = read(new_socket, buffer, 1024)) > 0) {
-      printf("Je rentre dans le serveur.\n");
-      printf("Le chemin est %s\n", buffer);
-      checked_wr(write(new_socket, buffer, lu) < 0);
-  }
-  
-  close(server_fd);
-  close(new_socket);
-
-
-
-
-  return 0;
 }
