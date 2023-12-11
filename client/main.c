00b0b662f4906f243a09a94f3a91604204a752d4
Marouane-124 <marouanemoussaoui221@gmail.com>
2023-12-11 16:25:51 +0100

    thread de haytam

--- client/main.c
+++ client/main.c
@@ -3,37 +3,57 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <signal.h>
+#include <pthread.h>
 #include "../commun/commun.h"
-#include <asm-generic/socket.h>
 
-
-int main(){
+void client_socket(int* client_Sock) {
     int sock = checked(socket(AF_INET, SOCK_STREAM, 0));
 
-    struct sockaddr_in address;   //Adresse du serveur du socket
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons(5555);
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(5555);
 
     inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
-    checked(connect(sock, (struct sockaddr *)&address, sizeof(address)));
 
-    char chemin[1000];
+    client_Sock = checked(connect(sock, (struct sockaddr)&address, sizeof(address)));
 
+    char chemin[1001];
+    int taille = 0;
 
-    while (fgets(chemin, sizeof(chemin), stdin) != NULL){
-        chemin[strlen(chemin)-1] = '\0';    
+    while (fgets(chemin, sizeof(chemin), stdin) != NULL) {
+        chemin[strlen(chemin) - 1] = '\0';
+        checked_wr(write(client_Sock, chemin, strlen(chemin) + 1));
+        ++taille;
 
         printf("Envoi...\n");
-        checked_wr(write(sock, chemin, strlen(chemin)+1));
-
     }
+
     close(sock);
-    return 0;
 }
 
+int main() {
+    const int MAX_CLIENTS = 1001;
+    pthread_t threads[MAX_CLIENTS];
+    int client_sockets[MAX_CLIENTS];
+    int nombre_de_client = 0;
 
+    while (nombre_de_client < MAX_CLIENTS) {
+        if (pthread_create(&threads[nombre_de_client], NULL, (void)client_socket, (void*)&client_sockets[nombre_de_client]) != 0) {
+            perror("Erreur lors de la création du thread");
+            close(client_sockets[nombre_de_client]);
+        } else {
+            nombre_de_client++;
+        }
+    }
 
+    for (int i = 0; i < nombre_de_client; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 
+    for (int i = 0; i < nombre_de_client; ++i) {
+        close(client_sockets[i]);
+    }
 
+    return 0;
+}
