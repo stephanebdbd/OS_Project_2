d9c93b6a9c323c354f8b6fbec00cb762e91b7677
TBKNbr1 <stephane.badi.budu@ulb.be>
2023-12-11 19:03:08 +0100

    Threads fonctionnent problème pour le client

--- client/main.c
+++ client/main.c
@@ -6,54 +6,49 @@
 #include <pthread.h>
 #include "../commun/commun.h"
 
-void client_socket(int* client_Sock) {
-    int sock = checked(socket(AF_INET, SOCK_STREAM, 0));
-
+void client_socket(int client_Sock) {
+    client_Sock = checked(socket(AF_INET, SOCK_STREAM, 0));
+    printf("ca passe");
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(5555);
-
+    
     inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
-
-    client_Sock = checked(connect(sock, (struct sockaddr)&address, sizeof(address)));
-
+    
+    checked(connect(client_Sock, (struct sockaddr *)&address, sizeof(address)));
+    
     char chemin[1001];
-    int taille = 0;
 
     while (fgets(chemin, sizeof(chemin), stdin) != NULL) {
         chemin[strlen(chemin) - 1] = '\0';
-        checked_wr(write(client_Sock, chemin, strlen(chemin) + 1));
-        ++taille;
-
         printf("Envoi...\n");
+        checked_wr(write(client_Sock, chemin, strlen(chemin) + 1));
     }
 
-    close(sock);
+    close(client_Sock);
+    return ;
 }
 
 int main() {
-    const int MAX_CLIENTS = 1001;
-    pthread_t threads[MAX_CLIENTS];
-    int client_sockets[MAX_CLIENTS];
-    int nombre_de_client = 0;
+   const int MAX_CLIENTS = 1001;
+   pthread_t threads; 
+   int client_sockets[MAX_CLIENTS];
+   int nombre_de_client = 0;
+   printf("ca passe");
+   while (nombre_de_client < MAX_CLIENTS) {
+      printf("ca passe");
+      if (pthread_create(&threads, NULL, client_socket, (void*)&client_sockets[nombre_de_client]) != 0) {
+         perror("Erreur lors de la création du thread");
+         close(client_sockets[nombre_de_client]);
+      } else {
+         nombre_de_client++;
+      }
+   }
 
-    while (nombre_de_client < MAX_CLIENTS) {
-        if (pthread_create(&threads[nombre_de_client], NULL, (void)client_socket, (void*)&client_sockets[nombre_de_client]) != 0) {
-            perror("Erreur lors de la création du thread");
-            close(client_sockets[nombre_de_client]);
-        } else {
-            nombre_de_client++;
-        }
-    }
+   for (int i = 0; i < nombre_de_client; ++i) {
+      pthread_join(threads, NULL);
+   }
 
-    for (int i = 0; i < nombre_de_client; ++i) {
-        pthread_join(threads[i], NULL);
-    }
-
-    for (int i = 0; i < nombre_de_client; ++i) {
-        close(client_sockets[i]);
-    }
-
-    return 0;
+   return 0;
 }
--- serveur/main.c
+++ serveur/main.c
@@ -1,59 +1,179 @@
-#include <arpa/inet.h>
+/**
+ * Code du serveur 
+ * .
+ **/
+
+
+
 #include <stdio.h>
-#include <string.h>
+#include <inttypes.h>
+#include <signal.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <pthread.h>
 #include "../commun/commun.h"
+#include <asm-generic/socket.h>
+#include "imgdist.h"
 
-void client_socket(int* client_Sock) {
-    int sock = checked(socket(AF_INET, SOCK_STREAM, 0));
+void ExempleSignaux(void);
 
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(5555);
+struct image {
+   char imagePath[1024];
+   char chemin[1000]; 
+   int distance;
+   uint64_t imageHash;  
+   uint64_t hash2;  
+   int Bestdistance;
+   char Bestchemin[1000];
+   };
 
-    inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
 
-    client_Sock = checked(connect(sock, (struct sockaddr)&address, sizeof(address)));
+void* compare_image(void *ptr) {
+   struct image *toute_img_comp = (struct image*)ptr;
 
-    char chemin[1001];
-    int taille = 0;
+   toute_img_comp->distance = DistancePHash(toute_img_comp->imageHash, toute_img_comp->hash2);
 
-    while (fgets(chemin, sizeof(chemin), stdin) != NULL) {
-        chemin[strlen(chemin) - 1] = '\0';
-        checked_wr(write(client_Sock, chemin, strlen(chemin) + 1));
-        ++taille;
-
-        printf("Envoi...\n");
-    }
-
-    close(sock);
+   printf("La distance entre les images est : %u\n", toute_img_comp->distance);
+   if (toute_img_comp->distance < toute_img_comp-> Bestdistance){
+      toute_img_comp->Bestdistance=toute_img_comp->distance;
+      strcpy(toute_img_comp->Bestchemin,toute_img_comp->chemin);
+   }
+   return NULL;
 }
 
-int main() {
-    const int MAX_CLIENTS = 1001;
-    pthread_t threads[MAX_CLIENTS];
-    int client_sockets[MAX_CLIENTS];
-    int nombre_de_client = 0;
 
-    while (nombre_de_client < MAX_CLIENTS) {
-        if (pthread_create(&threads[nombre_de_client], NULL, (void)client_socket, (void*)&client_sockets[nombre_de_client]) != 0) {
-            perror("Erreur lors de la création du thread");
-            close(client_sockets[nombre_de_client]);
-        } else {
-            nombre_de_client++;
-        }
-    }
 
-    for (int i = 0; i < nombre_de_client; ++i) {
-        pthread_join(threads[i], NULL);
-    }
 
-    for (int i = 0; i < nombre_de_client; ++i) {
-        close(client_sockets[i]);
-    }
 
-    return 0;
+
+
+int main(int argc, char* argv[]){
+    
+   int server_fd = checked(socket(AF_INET, SOCK_STREAM, 0));
+   
+   int opt = 1;
+   setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+   
+   struct sockaddr_in address;   //Adresse du serveur du socket
+   address.sin_family = AF_INET;
+   address.sin_addr.s_addr = INADDR_ANY;
+   address.sin_port = htons(5555);
+   
+   
+   checked(bind(server_fd, (struct sockaddr *)&address, sizeof(address))); //Lie l'adresse au socket
+   
+   checked(listen(server_fd, 10)); // mise en écoute de l'utilisateur
+   
+   
+   size_t addrlen = sizeof(address);
+   int new_socket = checked(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
+   
+
+   int lu;
+   pthread_t t1, t2, t3;
+   
+   struct image uneImage;
+   uneImage.Bestdistance=78;
+   
+   while ((lu = read(new_socket, uneImage.imagePath, 1024)) > 0) {
+      printf("Je rentre dans le serveur.\n");
+      printf("Le chemin est %s\n", uneImage.imagePath);
+      
+      if (!PHash(uneImage.imagePath, &uneImage.imageHash))
+         return 1;
+
+      FILE *listing = popen("./list-file ./img/", "r");
+      if (listing == NULL) {
+         perror("Erreur lors de l'ouverture du processus");
+         exit(EXIT_FAILURE);
+      }
+
+      int taille =0;
+      while (fgets(uneImage.chemin, sizeof(uneImage.chemin), listing) != NULL) {
+         uneImage.chemin[strlen(uneImage.chemin)-1] = '\0';
+         printf("Le chemin de l'image est : %s\n", uneImage.chemin);   //il faut encore ajouter chaque line à imagesBank
+
+         if (!PHash(uneImage.chemin, &uneImage.hash2))
+            return 1;
+
+         if (taille % 3 == 0) {
+               pthread_create(&t1, NULL,compare_image, (void*)&uneImage);
+               pthread_join(t1, NULL);
+         }else if (taille % 3 == 1) {
+               pthread_create(&t2, NULL, compare_image, (void*)&uneImage);
+               pthread_join(t2, NULL);
+         }else {
+               pthread_create(&t3, NULL, compare_image, (void*)&uneImage);
+               pthread_join(t3, NULL);
+         }
+         taille++;
+         
+      }
+      
+      pclose(listing);
+      ExempleSignaux();
+
+
+
+      checked_wr(write(new_socket, uneImage.imagePath, lu) < 0);
+      
+
+      
+      // Calcule du code de hachage perceptif de l'image reçue via le socket par le client et
+      // conservation de celui-ci dans imageHash.
+      
+   }
+   close(server_fd);
+   close(new_socket);
+
+
+
+
+   return 0;
+}
+
+static volatile sig_atomic_t signalRecu = 0;
+void SignalHandler(int sig) {
+   signalRecu = 1;
+}
+
+void ExempleSignaux(void) {
+   
+   /// Exemple gestion de signaux (cf Annexe de l'énoncé & corrigé du projet 1) ///
+   
+   // Forcer l'interruption des appels systèmes lors de la réception de SIGINT
+   struct sigaction action;
+   action.sa_handler = SignalHandler;
+   sigemptyset(&action.sa_mask);
+
+   if (sigaction(SIGINT, &action, NULL) < 0) {
+      perror("sigaction()");
+      return;
+   }
+   
+   
+   // Gestion idéale (court et sans risque d'accès concurrents) d'un signal
+   // (cf SignalHandler() également).
+   printf("Signal recu : %d.\n", signalRecu);
+   raise(SIGINT);
+   printf("Signal recu : %d.\n", signalRecu);
+   
+   
+   // Bloquer des signaux pour le thread courant
+   sigset_t set;
+    
+   sigemptyset(&set);        // Ensemble vide de signaux
+   sigaddset(&set, SIGINT);  // Ajouter le signal SIGINT
+   sigaddset(&set, SIGUSR1); // Ajouter le signal SIGUSR1
+    
+   if (pthread_sigmask(SIG_BLOCK, &set, NULL) != 0) {
+      perror("pthread_sigmask()");
+      return;
+   }
+   
+   /// ///
 }
