0c12732046403a151d8d58de156f9029d9e9dea5
Marouane-124 <marouanemoussaoui221@gmail.com>
2023-12-11 16:27:30 +0100

    thread de haytam serveur et client

--- serveur/main.c
+++ serveur/main.c
@@ -1,147 +1,59 @@
-/**
- * Code du serveur 
- * .
- **/
-
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <inttypes.h>
-#include <signal.h>
-#include "imgdist.h"
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "../commun/commun.h"
-#include <asm-generic/socket.h>
 
+void client_socket(int* client_Sock) {
+    int sock = checked(socket(AF_INET, SOCK_STREAM, 0));
 
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(5555);
 
-void ExempleSignaux(void);
+    inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
 
+    client_Sock = checked(connect(sock, (struct sockaddr)&address, sizeof(address)));
 
-int main(){
-   int server_fd = checked(socket(AF_INET, SOCK_STREAM, 0));
+    char chemin[1001];
+    int taille = 0;
 
-   int opt = 1;
-   setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    while (fgets(chemin, sizeof(chemin), stdin) != NULL) {
+        chemin[strlen(chemin) - 1] = '\0';
+        checked_wr(write(client_Sock, chemin, strlen(chemin) + 1));
+        ++taille;
 
-   struct sockaddr_in address;   //Adresse du serveur du socket
-   address.sin_family = AF_INET;
-   address.sin_addr.s_addr = INADDR_ANY;
-   address.sin_port = htons(5555);
+        printf("Envoi...\n");
+    }
 
-
-   checked(bind(server_fd, (struct sockaddr *)&address, sizeof(address))); //Lie l'adresse au socket
-
-   checked(listen(server_fd, 10)); // mise en écoute de l'utilisateur
-
-
-   size_t addrlen = sizeof(address);
-   int new_socket = checked(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
-  
-   char imagePath[1024];
-   int lu;
-   uint64_t imageHash, hash2;
-
-
-   while ((lu = read(new_socket, imagePath, 1024)) > 0) {
-      printf("Je rentre dans le serveur.\n");
-      printf("Le chemin est %s\n", imagePath);
-
-
-      // Calcule du code de hachage perceptif de l'image reçue via le socket par le client et
-      // conservation de celui-ci dans imageHash.
-      if (!PHash(imagePath, &imageHash))
-         return 1; // Échec dans le chargement de l'image (message sur stderr automatique)
-
-        // Idem pour "img/2.bmp".
-      if (!PHash("img/2.bmp", &hash2))
-         return 1; 
-
-      
-      FILE *listing = popen("./list-file ./img/", "r");
-      if (listing == NULL) {
-         perror("Erreur lors de l'ouverture du processus");
-         exit(EXIT_FAILURE);
-      }
-
-      const char *imagesBank;
-
-      char line[1000];
-      while (fgets(line, sizeof(line), listing) != NULL) {
-         line[strlen(line)-1] = '\0';
-         printf("Le chemin de l'image est : %s\n", line);   //il faut encore ajouter chaque line à imagesBank
-      }
-
-      pclose(listing);
-
-
-      // Calculer la distance entre hash1 et hash2
-      unsigned int distance = DistancePHash(imageHash, hash2); 
-
-      // Afficher la distance entre les deux images (valeur de retour d'img-dist dans le projet 1
-      // quand il n'y avait pas d'erreur).
-      printf("La distance entre les images est : %d\n", distance);
-   
-      /// Exemple gestion de signaux (cf Annexe de l'énoncé & corrigé du projet 1) ///
-   
-      ExempleSignaux();
-   
-      /// ///
-
-
-      checked_wr(write(new_socket, imagePath, lu) < 0);
-  }
-  
-   close(server_fd);
-   close(new_socket);
-
-
-
-
-  return 0;
+    close(sock);
 }
 
-static volatile sig_atomic_t signalRecu = 0;
-void SignalHandler(int sig) {
-   signalRecu = sig;
-}
+int main() {
+    const int MAX_CLIENTS = 1001;
+    pthread_t threads[MAX_CLIENTS];
+    int client_sockets[MAX_CLIENTS];
+    int nombre_de_client = 0;
 
-void ExempleSignaux(void) {
+    while (nombre_de_client < MAX_CLIENTS) {
+        if (pthread_create(&threads[nombre_de_client], NULL, (void)client_socket, (void*)&client_sockets[nombre_de_client]) != 0) {
+            perror("Erreur lors de la création du thread");
+            close(client_sockets[nombre_de_client]);
+        } else {
+            nombre_de_client++;
+        }
+    }
 
-   /// Exemple gestion de signaux (cf Annexe de l'énoncé & corrigé du projet 1) ///
-   
-   // Forcer l'interruption des appels systèmes lors de la réception de SIGINT
-   struct sigaction action;
-   action.sa_handler = SignalHandler;
-   sigemptyset(&action.sa_mask);
+    for (int i = 0; i < nombre_de_client; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 
-   if (sigaction(SIGINT, &action, NULL) < 0) {
-      perror("sigaction()");
-      return;
-   }
-   
-   
-   // Gestion idéale (court et sans risque d'accès concurrents) d'un signal
-   // (cf SignalHandler() également).
-   printf("Signal recu : %d.\n", signalRecu);
-   raise(SIGINT);
-   printf("Signal recu : %d.\n", signalRecu);
-   
-   
-   // Bloquer des signaux pour le thread courant
-   sigset_t set;
-    
-   sigemptyset(&set);        // Ensemble vide de signaux
-   sigaddset(&set, SIGINT);  // Ajouter le signal SIGINT
-   sigaddset(&set, SIGUSR1); // Ajouter le signal SIGUSR1
-    
-   if (pthread_sigmask(SIG_BLOCK, &set, NULL) != 0) {
-      perror("pthread_sigmask()");
-      return;
-   }
-   
-   /// ///
+    for (int i = 0; i < nombre_de_client; ++i) {
+        close(client_sockets[i]);
+    }
+
+    return 0;
 }
