5a6e0f21be0c92697be5d94e7ea7425f250403c5
TBKNbr1 <stephane.badi.budu@ulb.be>
2023-12-16 12:58:53 +0100

    le main du serveur a été nettoyé

--- serveur/main.c
+++ serveur/main.c
@@ -17,6 +17,7 @@
 #include <asm-generic/socket.h>
 #include "imgdist.h"
 
+static volatile sig_atomic_t signalRecu = 0;
 struct image meilleure_image;
 
 void ExempleSignaux(void);
@@ -34,11 +35,7 @@ void* compare_image(void *ptr) {
    return NULL;
 }
 
-
-
-int main(){
-   struct client client;
-   struct to_compare_image to_compare[3];
+int getPictures(struct to_compare_image* to_compare){
    FILE *listing = popen("./list-file img", "r");
    if (listing == NULL) {
       perror("Erreur lors de l'ouverture du processus");
@@ -50,25 +47,33 @@ int main(){
    while ((fgets(to_compare[i].librairie[to_compare[i].longueur].chemin, sizeof(to_compare[i].librairie[to_compare[i].longueur].chemin), listing) != NULL)){
       to_compare[i].librairie[to_compare[i].longueur].chemin[strlen(to_compare[i].librairie[to_compare[i].longueur].chemin)-1] = '\0';
       if (!PHash(to_compare[i].librairie[to_compare[i].longueur].chemin, &to_compare[i].librairie[to_compare[i].longueur].hash))
-         return 1;
+         return 0;
       to_compare[i].longueur++;
       i += (to_compare[i].longueur == 34) ? 1 : 0;
    }
    pclose(listing);
-   int server_fd = checked(socket(AF_INET, SOCK_STREAM, 0));
-   int opt = 1;
-   setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-   struct sockaddr_in address;   //Adresse du serveur du socket
-   address.sin_family = AF_INET;
-   address.sin_addr.s_addr = INADDR_ANY;
-   address.sin_port = htons(5555);
-   checked(bind(server_fd, (struct sockaddr *)&address, sizeof(address))); //Lie l'adresse au socket
-   checked(listen(server_fd, 10)); // mise en écoute de l'utilisateur
-   size_t addrlen = sizeof(address);
-   int new_socket = checked(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
-   pthread_t t1, t2, t3;
-   int lu;
-   while ((lu = read(new_socket, &client, sizeof(struct client))) > 0) {
+   return 1;
+}
+
+void create_socket(int* server_fd, int* new_socket){
+      *server_fd = checked(socket(AF_INET, SOCK_STREAM, 0));
+      int opt = 1;
+      setsockopt(*server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+      struct sockaddr_in address;   
+      address.sin_family = AF_INET;
+      address.sin_addr.s_addr = INADDR_ANY;
+      address.sin_port = htons(5555);
+      checked(bind(*server_fd, (struct sockaddr *)&address, sizeof(address))); 
+      checked(listen(*server_fd, 10)); 
+      size_t addrlen = sizeof(address);
+      *new_socket = checked(accept(*server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
+}
+
+void connetToClient(struct to_compare_image* to_compare, int new_socket){
+      struct client client;
+      pthread_t t1, t2, t3;
+      int lu;
+      while ((lu = read(new_socket, &client, sizeof(struct client))) > 0) {
       meilleure_image.distance = 64;
       if (PHashRaw(client.contenuImage, client.taille, &client.hash)){
          for (int i=0; i < 3; i++)
@@ -81,54 +86,41 @@ int main(){
          pthread_join(t3, NULL);
       }
       checked_wr(write(new_socket, &meilleure_image, sizeof(meilleure_image)));
-      
+      }
+}
 
-   }
+int main(){
+   struct to_compare_image to_compare[3];
+   if (!getPictures(to_compare))
+      return EXIT_FAILURE;
+   int server_fd, new_socket;
+   create_socket(&server_fd, &new_socket);
+   connetToClient(to_compare, new_socket);
    close(server_fd);
    close(new_socket);
    ExempleSignaux();
    return 0;
 }
 
-
-static volatile sig_atomic_t signalRecu = 0;
 void SignalHandler(int sig) {
    signalRecu = sig;
 }
 
 void ExempleSignaux(void) {
-   
-   /// Exemple gestion de signaux (cf Annexe de l'énoncé & corrigé du projet 1) ///
-   
-   // Forcer l'interruption des appels systèmes lors de la réception de SIGINT
    struct sigaction action;
    action.sa_handler = SignalHandler;
    sigemptyset(&action.sa_mask);
-
    if (sigaction(SIGINT, &action, NULL) < 0) {
       perror("sigaction()");
       return;
    }
-   
-   
-   // Gestion idéale (court et sans risque d'accès concurrents) d'un signal
-   // (cf SignalHandler() également).
-   printf("Signal recu : %d.\n", signalRecu);
    raise(SIGINT);
-   printf("Signal recu : %d.\n", signalRecu);
-   
-   
-   // Bloquer des signaux pour le thread courant
    sigset_t set;
-    
-   sigemptyset(&set);        // Ensemble vide de signaux
-   sigaddset(&set, SIGINT);  // Ajouter le signal SIGINT
-   sigaddset(&set, SIGUSR1); // Ajouter le signal SIGUSR1
-    
+   sigemptyset(&set);        
+   sigaddset(&set, SIGINT);  
+   sigaddset(&set, SIGUSR1); 
    if (pthread_sigmask(SIG_BLOCK, &set, NULL) != 0) {
       perror("pthread_sigmask()");
       return;
    }
-   
-   /// ///
 }
